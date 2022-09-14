073ca3bab3498f30ef5d0c86ad823f014634fc18
Elirector <elirector@gmail.com>
2022-09-14 10:07:13 +0300

    GUZZLE

--- index.php
+++ index.php
@@ -1,45 +1,23 @@
 <?php
 require 'vendor/autoload.php';
-$postparam = json_encode(["answer"=>42]);
-$curl = curl_init("https://reqbin.com/echo/post/json");
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, ["Content-type:application/json","Content-length:".strlen($postparam)]);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $postparam);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-$content = curl_exec($curl);
-$curlres = curl_getinfo($curl);
-curl_close($curl);
-$data = json_decode($content);
+use GuzzleHttp\Client;
+
+$guzzle = new Client(["base_uri"=>"http://httpbin.org", "timeout"=>2.0]);
+
+$response = $guzzle->post("/post", ["form_params"=>["data"=>"test 42"]]);
+$data = json_decode(json_encode($response), true);
+$code = $response->getStatusCode(); // 200
+$reason = $response->getReasonPhrase(); // OK
+echo $code, " ", $reason;
+echo $response->getBody()."\r\n";
+
+
+
+
+
 
 ?>
-
-<html lang="en-US">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <title>Curl Demo</title>
-</head>
-<body>
-<div><?php var_dump($content);  ?></div>
-<div><?php var_dump($data);  ?></div>
-<div><?php var_dump($curlres);  ?></div>
-<table>
-    <thead>
-    <tr>
-        <td colspan="2">Curl Demo</td>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($data as $key => $item): ?>
-        <tr>
-            <td><?= $key ?></td>
-            <td><?= $item ?></td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
-</body>
-</html>
 <?php
 /*
 * wttr.in/Kazan ???
